4d60b58506cd3de249f91a56a9a711a7c0dab18d
plm66 <forestry@miondo.com>
2023-08-18 00:49:55 +0200

    on ajoute les fichiers pour chaque categorie de commandes mode

--- stream.py
+++ stream.py
@@ -51,3 +51,17 @@ elif option3:
     for link in selected_command['documentation_links']:
         st.write(link)
 
+cheat_sheet_links = [
+    "https://phoenixnap.com/kb/wp-content/uploads/2021/11/vim-commands-cheat-sheet-by-pnap.pdf",
+    "https://link-to-another-cheat-sheet.com",
+    # Add more links as needed
+]
+
+cheat_sheet_html = "<div style='background-color: #f0f0f0; padding: 20px; border-radius: 10px;'>"
+cheat_sheet_html += "<h3 style='color: #333;'>Cheat Sheets:</h3><ul>"
+for link in cheat_sheet_links:
+    cheat_sheet_html += f"<li><a href='{link}' target='_blank' style='color: #007bff;'>{link}</a></li>"
+cheat_sheet_html += "</ul></div>"
+
+st.markdown(cheat_sheet_html, unsafe_allow_html=True)
+
